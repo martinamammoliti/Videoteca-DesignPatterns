e9ca7977a97a04594b79b3a0cd48b7ab7f68513c
Martina Mammoliti <marti.mammoliti@gmail.com>
2026-06-16 17:03:38 +0200

    feat: implementato pattern Observer (Interfaccia, VistaTabellare e aggiornato Model)

--- model/Videoteca.java
+++ model/Videoteca.java
@@ -1,8 +1,26 @@
+package model;
 import java.util.ArrayList;
+import java.util.List;
+import view.Observer;
 
 public class Videoteca {
     private final List<FilmIF> elencoFilm=new ArrayList<>();
-    
+    private final List<Observer> observers=new ArrayList<>();
+
+    public void attach(Observer o){
+        if(o!=null && !observers.contains(o))
+            observers.add(o);
+    }
+
+    public void detach(Observer o){
+        observers.remove(o);
+    }
+
+    public void notifyObservers(){
+        for(Observer o:observers)
+            o.update();
+    }
+
     public void inserisci(FilmIF film){
         elencoFilm.add(film);
         System.out.println("Film inserito: "+film.getTitolo());
@@ -21,6 +39,13 @@ public class Videoteca {
     public void elimina(int id){
         elencoFilm.removeIf(film -> film.getId()==id);
         System.out.println("Film eliminato con ID: "+id);
+    }
+
+    public void setElenco(List<FilmIF> nuovaLista){
+        this.elencoFilm.clear();
+        this.elencoFilm.addAll(nuovaLista);
+        System.out.println("Intero catalogo ripristinato/sostituito.");
+        notifyObservers();
     }
 
     public List<FilmIF> getElenco(){
---
+++ view/VistaTabellare.java
@@ -0,0 +1,34 @@
+package view;
+
+import model.Videoteca;
+
+public class VistaTabellare implements Observer{
+    private final Videoteca videoteca;
+
+    public VistaTabellare(Videoteca videoteca){
+        this.videoteca=videoteca;
+        this.videoteca.attach(this);
+    }
+
+    @Override
+    public void update(){
+        System.out.println("\n[UI] Riceuto segnale di update. Richiedo i dati...");
+        List<FilmIF> datiAggiornati=videoteca.getElenco();
+        mostra(datiAggiornati);
+    }
+
+    private void mostra(List<FilmIF> lista){
+        System.out.println("====== CATALOGO VIDEOTECA AGGIORNATO =======");
+        if(lista.isEmpty())
+            System.out.println("Il catalogo è vuoto.");
+        else{
+            for(FilmIF f:lista){
+                System.out.println(String.format("[%d] %s - Regia: %s (%d) | [%s] Voto: %d/10", 
+                    f.getId(), f.getTitolo(), f.getRegista, f.getAnnoUscita(), f.getStatoVisione(), f.getValutazione()
+                ));
+            }
+        }
+        System.out.println("====================================================\n");
+    }
+
+}
